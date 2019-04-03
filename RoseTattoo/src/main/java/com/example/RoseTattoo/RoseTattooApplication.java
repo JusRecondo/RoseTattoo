5ac2cd2305e36053038e31daba943f4b47a27a4f
JusRecondo <justina.recondo@gmail.com>
2019-04-02 23:07:50 -0300

    primer commit

---
+++ RoseTattoo/src/main/java/com/example/RoseTattoo/RoseTattooApplication.java
@@ -0,0 +1,13 @@
+package com.example.RoseTattoo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RoseTattooApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RoseTattooApplication.class, args);
+	}
+
+}
